b5e1cc930318e588284d028229fb50606e73bea5
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-04-18 22:59:56 +0000

    Modified dialog.js
    Added close listener when dialog backdrop is clicked

--- src/scripts/dialog.js
+++ src/scripts/dialog.js
@@ -1,9 +1,32 @@
 const modal = document.getElementById('modal')
-const openModal = document.getElementById('send')
+// const openModal = document.getElementById('send')
 const closeModal = document.getElementById('close-btn')
+const complainForm = document.forms["complain"]
 
-openModal.addEventListener('click', () => {
+// openModal.addEventListener('click', () => {
+//     let username = complainForm["username"].value
+//     let message = complainForm["message"].value
+
+//     if(username == "" || message == ""){
+//         console.log('empty')
+//     }else {
+//         modal.showModal()
+//     }
+// })
+
+function openModal() {
     modal.showModal()
+    return false
+}
+
+modal.addEventListener('click', (e) => {
+   if(e.target.nodeName === "DIALOG") {
+    modal.setAttribute('close', '')
+    modal.addEventListener('animationend', () => {
+        modal.removeAttribute('close')
+        modal.close()
+    }, {once: true})
+   }
 })
 
 closeModal.addEventListener('click', () => {
